5238cd790abaa187d3f65200b355b8303647df6a
snqre <marco.bizxaro@gmail.com>
2025-08-13 14:01:05 +0200

    update layout

--- src/layout/grid_item.rs
+++ src/layout/grid_item.rs
@@ -4,10 +4,10 @@ use super::*;
 #[derive(Clone)]
 #[derive(PartialEq)]
 pub struct GridItemProps {
-    pub from_x: u8,
-    pub from_y: u8,
-    pub to_x: u8,
-    pub to_y: u8,
+    pub x_from: u8,
+    pub y_from: u8,
+    pub x_to: u8,
+    pub y_to: u8,
     pub z: Option<u128>,
     pub class: Option<String>,
     pub style: Option<String>,
@@ -26,10 +26,10 @@ pub fn GridItem(props: GridItemProps) -> Element {
                     z-index: {};
                     {}
                 "#,
-                props.from_x,
-                props.to_x,
-                props.from_y,
-                props.to_y,
+                props.x_from,
+                props.x_to,
+                props.y_from,
+                props.y_to,
                 props.z.unwrap_or_default(),
                 props.style.unwrap_or_default()
             ),
